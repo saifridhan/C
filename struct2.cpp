3cf33b46fde81bb95623b1c09af524a044e20112
saifridhan <125675058+saifridhan@users.noreply.github.com>
2023-02-17 20:36:01 +0530

    Add files via upload

---
+++ struct2.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+struct A
+{
+	int a;
+	float b;
+};
+int main()
+{
+	struct A ob;
+	
+	printf("enter the number");
+	scanf("%d%f",&a,&b);
+	int c=(ob.a+ob.b);
+	printf("%f",c);
+}
